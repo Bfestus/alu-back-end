f8c63005683cbec1854b99f9efe30c210325df10
Bfestus <f.bigiriman@alustudent.com>
2023-11-04 09:24:15 +0000

    tasks

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,77 +1,66 @@
 #!/usr/bin/python3
 """
-This script fetches and displays the TODO list and exports it to a CSV file.
+script fetches and displays the TODO list and exports it to a CSV file.
 """
 
-import sys
 import requests
+import sys
 import csv
 
-
-def get_employee_todo_progress(employee_id):
+def fetch_todo_list_progress(employee_id):
     """
-    Fetch and display the TODO list progress for a given employee ID.
+    Fetches TODO list progress for a given employee ID and exports data in CSV format.
 
-    :param employee_id: Integer representing the employee ID.
+    Args:
+        The ID of the employee whose TODO list progress is to be fetched.
+
+    Returns:
+        None
     """
-    # Base URL for the JSONPlaceholder API
-    base_url = 'https://jsonplaceholder.typicode.com'
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
 
-    # Endpoint for user details
-    user_endpoint = f'{base_url}/users/{employee_id}'
+    # Fetch user data from the API
+    user_response = requests.get(user_url)
+    # Fetch TODO list data for the specific user from the API
+    todos_response = requests.get(todos_url)
 
-    # Endpoint for user's TODO list
-    todo_endpoint = f'{base_url}/todos?userId={employee_id}'
+    # Check if API requests were successful
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        return
 
-    try:
-        # Fetch user details
-        user_response = requests.get(user_endpoint)
-        u_data = user_response.json()
+    # Parse JSON responses
+    user_data = user_response.json()
+    todos_data = todos_response.json()
 
-        # Fetch user's TODO list
-        todo_response = requests.get(todo_endpoint)
-        todo_data = todo_response.json()
+    # Prepare data for CSV export
+    csv_data = []
+    for task in todos_data:
+        task_completed_status = "True" if task['completed'] else "False"
+        csv_row = [user_data['id'], user_data['username'], task_completed_status, task['title']]
+        csv_data.append(csv_row)
 
-        # Display progress information
-        employee_name = u_data.get('username', 'Unknown')
-        print(f"Employee Name: {employee_name}")
+    # Export data to CSV file
+    file_name = f"{user_data['id']}.csv"
+    with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csvwriter.writerows(csv_data)
 
-        # Export tasks to CSV
-        csv_file_name = f"{employee_id}.csv"
-        with open(csv_file_name, 'w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow
-            (["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-            # Write each task to the CSV file
-            for task in todo_data:
-                task_completed_status = "Completed" if task['completed'] 
-                                                    else "Not Completed"
-                csv_writer.writerow([
-                    employee_id,
-                    employee_name,
-                    task_completed_status,
-                    task['title']
-                ])
-
-                # Display titles of completed tasks
-                if task['completed']:
-                    print(f"\t{task['title']}")
-
-        print(f"Tasks exported to {csv_file_name}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
+    print(f"Data exported to {file_name}")
 
 if __name__ == "__main__":
-    # Check if an employee ID is provided as a command-line argument
+    # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+        print("Usage: python script_name.py <employee_id>")
+    else:
+        try:
+            # Parse employee ID from the command-line argument
+            employee_id = int(sys.argv[1])
+            # Fetch TODO list progress for the given employee ID and export data to CSV
+            fetch_todo_list_progress(employee_id)
+        except ValueError:
+            print("Error: Invalid employee ID. Please provide a valid integer.")
 
-    # Get employee ID from the command-line argument
-    employee_id = int(sys.argv[1])
-
-    # Call the function to get and display employee TODO list
-    get_employee_todo_progress(employee_id)
