0b89a4f2c51228111c08ea62d76675fa64ce8fcf
Bfestus <f.bigiriman@alustudent.com>
2023-11-04 09:13:21 +0000

    tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -36,7 +36,8 @@ def get_employee_todo_progress(employee_id):
         c_tasks = sum(task['completed'] for task in todo_data)
 
         # Display progress information
-        print(f"Employee Name: {u_data.get('name', 'Unknown')} ({c_tasks}/{t_tasks}):")
+        print(f"Employee Name: {u_data.get('name', 'Unknown')} 
+	({c_tasks}/{t_tasks}):")
 
         # Display titles of completed tasks
         for task in todo_data:
